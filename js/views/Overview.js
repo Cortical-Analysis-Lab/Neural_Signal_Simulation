98f0d714eeba5c4a77309c64bb816ced0686d2b7
Cortical-Analysis-Lab <NeuroResearch@sacredheart.edu>
2025-12-17 01:32:08 -0500

    Update Overview.js

--- js/views/Overview.js
+++ js/views/Overview.js
@@ -1,10 +1,4 @@
 function drawOverview(state) {
-  push();
-  fill(200);
-  textAlign(CENTER, CENTER);
-  textSize(24);
-  text("OVERVIEW VIEW", width / 2, height / 2 - 40);
-  textSize(14);
-  text("Whole neuron integration (placeholder)", width / 2, height / 2);
-  pop();
+  drawNeuron();
+  updateSynapseHover();
 }
