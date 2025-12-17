cc96574e8e754867668077a52c986e5e318a03c3
Cortical-Analysis-Lab <NeuroResearch@sacredheart.edu>
2025-12-17 01:32:48 -0500

    Update Overview.js

--- js/views/Overview.js
+++ js/views/Overview.js
@@ -2,3 +2,28 @@ function drawOverview(state) {
   drawNeuron();
   updateSynapseHover();
 }
+function drawNeuron() {
+  // Soma
+  push();
+  noStroke();
+  fill(180);
+  ellipse(0, 0, neuron.somaRadius * 2);
+  pop();
+
+  // Dendrites
+  stroke(150);
+  strokeWeight(4);
+  neuron.dendrites.forEach(d => {
+    const end = polarToCartesian(d.angle, d.length);
+    line(0, 0, end.x, end.y);
+  });
+
+  // Synaptic boutons
+  neuron.synapses.forEach(s => {
+    push();
+    noStroke();
+    fill(s.hovered ? 255 : 200);
+    ellipse(s.x, s.y, s.radius * 2);
+    pop();
+  });
+}
