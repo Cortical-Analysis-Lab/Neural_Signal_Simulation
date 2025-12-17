883c4e56ff5a67c2652c93ff730ee49ade3eb62b
Cortical-Analysis-Lab <NeuroResearch@sacredheart.edu>
2025-12-17 01:57:50 -0500

    Update epsp.js

--- js/signals/epsp.js
+++ js/signals/epsp.js
@@ -14,32 +14,21 @@ function spawnEPSP(synapse) {
   });
 }
 
-function updateEPSPs() {
-  for (let i = epsps.length - 1; i >= 0; i--) {
-    const e = epsps[i];
-    e.progress += e.speed;
-    e.amplitude *= e.decay;
-
-    if (e.progress >= 1 || e.amplitude < 1) {
-      epsps.splice(i, 1);
-    }
-  }
-}
-
 function drawEPSPs() {
+  if (!neuron.synapses || neuron.synapses.length === 0) return;
+
   epsps.forEach(e => {
     const syn = neuron.synapses[e.synapseId];
     const d = neuron.dendrites[e.synapseId];
+    if (!syn || !d) return;
 
     const start = polarToCartesian(d.angle, d.length);
-    const end = { x: 0, y: 0 };
-
-    const x = lerp(start.x, end.x, e.progress);
-    const y = lerp(start.y, end.y, e.progress);
+    const x = lerp(start.x, 0, e.progress);
+    const y = lerp(start.y, 0, e.progress);
 
     push();
     stroke(80, 150, 255); // EPSP BLUE
-    strokeWeight(map(e.amplitude, 6, 30, 3, 10));
+    strokeWeight(map(e.amplitude, 6, 30, 4, 10));
     point(x, y);
     pop();
   });
