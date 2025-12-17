f0e542fdaa6436f4c8fd8d02fcf55599ad018d4f
Cortical-Analysis-Lab <NeuroResearch@sacredheart.edu>
2025-12-17 00:30:53 -0500

    Create IonView.js

---
+++ js/views/IonView.js
@@ -0,0 +1,10 @@
+function drawIonView(state) {
+  push();
+  fill(120, 180, 255);
+  textAlign(CENTER, CENTER);
+  textSize(24);
+  text("ION CHANNEL VIEW", width / 2, height / 2 - 40);
+  textSize(14);
+  text("Na⁺ / K⁺ dynamics (placeholder)", width / 2, height / 2);
+  pop();
+}
