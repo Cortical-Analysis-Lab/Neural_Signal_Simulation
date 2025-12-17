1b91d899bcb56d57915b1d53f3bd80a0445d635f
Cortical-Analysis-Lab <NeuroResearch@sacredheart.edu>
2025-12-17 00:31:44 -0500

    Create SynapseView.js

---
+++ js/views/SynapseView.js
@@ -0,0 +1,10 @@
+function drawSynapseView(state) {
+  push();
+  fill(255, 180, 120);
+  textAlign(CENTER, CENTER);
+  textSize(24);
+  text("SYNAPSE VIEW", width / 2, height / 2 - 40);
+  textSize(14);
+  text("Vesicle release & receptors (placeholder)", width / 2, height / 2);
+  pop();
+}
