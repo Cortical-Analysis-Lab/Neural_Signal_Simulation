b1e070dd5ff0b5b1a913a6985807e99f1e629e7c
Cortical-Analysis-Lab <NeuroResearch@sacredheart.edu>
2025-12-17 01:41:46 -0500

    Update synapseInteraction.js

--- js/interaction/synapseInteraction.js
+++ js/interaction/synapseInteraction.js
@@ -1,6 +1,8 @@
 // =====================================================
-// SYNAPSE INTERACTION
+// SYNAPSE INTERACTION (RESIZE + ACTIVATE)
 // =====================================================
+
+let activeSynapse = null;
 
 function updateSynapseHover() {
   const mx = (mouseX - width / 2) / camera.zoom + camera.x;
@@ -8,6 +10,29 @@ function updateSynapseHover() {
 
   neuron.synapses.forEach(s => {
     const d = dist(mx, my, s.x, s.y);
-    s.hovered = d < s.radius + 4;
+    s.hovered = d < s.radius + 6;
   });
+}
+
+function mousePressed() {
+  neuron.synapses.forEach(s => {
+    if (s.hovered) {
+      activeSynapse = s;
+      spawnEPSP(s);
+    }
+  });
+}
+
+function mouseDragged() {
+  if (activeSynapse) {
+    activeSynapse.radius = constrain(
+      activeSynapse.radius + movedY * -0.1,
+      6,
+      30
+    );
+  }
+}
+
+function mouseReleased() {
+  activeSynapse = null;
 }
