eca0695acaf954d83949401fa8ad80c79af3a37c
Cortical-Analysis-Lab <NeuroResearch@sacredheart.edu>
2025-12-21 21:54:37 -0500

    Update Overview.js

--- js/views/Overview.js
+++ js/views/Overview.js
@@ -12,17 +12,7 @@ function drawOverview(state) {
   drawNeuron1();
   drawNeuron2();
 
-updateSynapseHover();
-updateEPSPs();
-updateSoma();
-updateVoltageTrace();
-updateAxonSpikes();
-updateTerminalDots();
-updateVesicles();
-updateNeuron2EPSPs();
-updateSynapticCoupling();
 
-  
   drawVoltageTrace();
   drawEPSPs();
   drawAxonSpikes();
