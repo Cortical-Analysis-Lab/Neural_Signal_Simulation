f5069a8be54bf00bd4467536fb2db046a33b1b8c
Cortical-Analysis-Lab <NeuroResearch@sacredheart.edu>
2025-12-26 20:51:06 -0500

    Update SynapseView.js

--- js/views/SynapseView.js
+++ js/views/SynapseView.js
@@ -39,8 +39,8 @@ function drawSynapseView() {
   drawAstrocyticEndfoot();
 
   // Neurons move independently
-  drawTNeuron(+140, 100, +1);  // ↑ closer to astrocyte
-  drawTNeuron(-140, 85, -1);
+  drawTNeuron(+140, 75, +1);  // ↑ closer to astrocyte
+  drawTNeuron(-140, 75, -1);
 
   pop();
 }
