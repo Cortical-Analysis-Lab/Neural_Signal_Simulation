5267f92b5d229338fdaf92282e4bc7a4c2f25996
Cortical-Analysis-Lab <NeuroResearch@sacredheart.edu>
2025-12-26 21:52:51 -0500

    Create postSynapse.js

---
+++ js/synapse/postSynapse.js
@@ -0,0 +1,11 @@
+console.log("🟡 postSynapse loaded");
+
+// =====================================================
+// POSTSYNAPTIC NEURON
+// =====================================================
+function drawPostSynapse() {
+  push();
+  translate(-140, 75);
+  drawTNeuronShape(-1);
+  pop();
+}
