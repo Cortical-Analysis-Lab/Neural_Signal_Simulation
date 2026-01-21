26bcd084d7d280fe50aa5c79d2c6cc5e05468e61
Cortical-Analysis-Lab <NeuroResearch@sacredheart.edu>
2026-01-20 21:50:08 -0500

    Update synapticBurst.js

--- js/synapse/physiology/synapticBurst.js
+++ js/synapse/physiology/synapticBurst.js
@@ -40,7 +40,7 @@ const NT_PER_FRAME_MIN = 1;
 const NT_PER_FRAME_MAX = 2;
 
 const NT_INITIAL_SPEED  = 0.26;
-const NT_INITIAL_SPREAD = 0.95;
+const NT_INITIAL_SPREAD = 1.2;
 
 const NT_ADVECT_X = 0.018;
 const NT_BROWNIAN = 0.003;
